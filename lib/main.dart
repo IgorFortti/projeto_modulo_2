c3533fb2fdfd3b674afb5e5d4b2240a2b58a3792
caiohxp <caiocesar.ccomp@gmail.com>
2024-03-11 20:12:02 -0300

    change title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ToDo List',
+      title: 'Somador',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'ToDo List'),
+      home: const MyHomePage(title: 'Somador'),
     );
   }
 }
