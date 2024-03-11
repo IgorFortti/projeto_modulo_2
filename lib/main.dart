a5567feea5e06ae6594a55f4b98b61cf5037003a
Igor Fortti <igormfortti@gmail.com>
2024-03-11 18:51:53 -0300

    Alterado conteúdo e estilo do texto

--- lib/main.dart
+++ lib/main.dart
@@ -105,8 +105,9 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text(
+              'Número atual de cliques:',
+              style:  Theme.of(context).textTheme.bodyLarge,
             ),
             Text(
               '$_counter',
